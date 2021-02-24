db554414fe2d42489b87378d273d240e5277d3ec
antbaobao <2929712050@qq.com>
2021-02-24 17:16:29 +0800

    style: 首页整理

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,17 +5,16 @@ module.exports = {
   head: [
     ["link", { rel: "icon", href: "/favicon.ico" }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
-  base: "/wl05.github.io/", // 这是部署到github相关的配置 下面会讲
+  base: "/wl05.github.io/",
   markdown: {
     lineNumbers: true, // 代码块显示行号
   },
   themeConfig: {
-    sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
-    lastUpdated: "Last Updated", // 文档更新时间：每个文件git最后提交的时间
+    sidebarDepth: 2,
+    lastUpdated: "最后更新于",
     nav: [
       { text: "Tech", link: "/tech/" },
       { text: "Life", link: "/life/" },
-      // 下拉列表
       {
         text: "GitHub",
         link: "https://github.com/wl05/ant-site",
