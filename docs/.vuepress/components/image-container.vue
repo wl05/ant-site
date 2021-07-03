aa05bcd4f4e2ec0bece3b64781d9889273277de4
antbaobao <2929712050@qq.com>
2021-07-03 21:16:35 +0800

    feat: 箱龟繁殖总结

---
+++ docs/.vuepress/components/image-container.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="container">
+<div class="img-container" :width="width">
+    <slot></slot>
+  </div>
+</div>
+  
+</template>
+<script>
+export default {
+  name: "image-container",
+  props:["width"]
+}
+</script>
+
+<style rel="stylesheet/scss" scoped>
+  .container {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+  }
+  .img-container {
+    width: 400px;
+  
+  }
+</style>
---
+++ docs/.vuepress/components/video-container.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="container">
+<video class="vedio-container" :width="width" controls>
+    <slot></slot>
+  </video>
+</div>
+  
+</template>
+<script>
+export default {
+  name: "vedio-container",
+  props:["width"]
+}
+</script>
+
+<style rel="stylesheet/scss" scoped>
+  .container {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+  }
+  .vedio-container {
+    width: 400px;
+  
+  }
+</style>
