62472f79a1885d323b1d03ae1945d2a9f24b8cf4
antbaobao <2929712050@qq.com>
2021-07-04 09:19:39 +0800

    feat: 部署更新

--- shell.sh
+++ shell.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-docker stop mynginx-image
-docker rm mynginx-container
-docker build -t mynginx-image .
-docker run \
--p 80:80 \
--d --name mynginx-container \
-mynginx-image
+cd docker
+docker-compose up -d
