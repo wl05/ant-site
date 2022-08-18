1ce00b77c6d8cb5e8c8f60ab7edc6aedf0f2d2fa
wangle <2929712050@qq.com>
2022-08-18 22:49:35 +0800

    .

--- docs/.vuepress/components/home.vue
+++ docs/.vuepress/components/home.vue
@@ -1,14 +1,17 @@
 <template>
-<div class="container">
-  <div class="content">
-    <h1>学习总结与记录</h1>  
-    <ul class="list">
-      <li><a href="/tech/css/css-box-model/">技术</a></li>  
-      <li><a href="/read/">读书</a></li>  
-    </ul>
-  </div>  
-</div>
-  
+  <div class="container">
+    <div class="content">
+      <h1>学习总结与记录</h1>
+      <ul class="list">
+        <li>
+          <a href="/tech/css/css-box-model/">技术</a>
+        </li>
+        <li>
+          <a href="/read/">读书</a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -18,18 +21,20 @@ export default {
 </script>
 
 <style rel="stylesheet/scss" scoped>
-  .container{
-    background: url("/bg.jpeg") no-repeat;
-    min-height: 100vh;
-    overflow: auto;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  .content {
-    color: white;
-  }
-  .list li a{
-    color: white;
-  }
+.container {
+  background: url('/bg.jpeg') no-repeat;
+  min-height: 100vh;
+  overflow: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-size: cover;
+  filter: rgb(254, 254, 254);
+}
+.content {
+  color: white;
+}
+.list li a {
+  color: white;
+}
 </style>
--- docs/tech/javascript/js-closure/index.md
+++ docs/tech/javascript/js-closure/index.md
@@ -1,7 +1,15 @@
 # Javascript 闭包
 
+## 闭包的定义
+
+## 产生闭包的场景
+
+1. 函数作为函数的返回值，并且返回的函数对
+2. 函数作为参数传递
+
 ## 参考资料
 
 * [Variable scope, closure](https://javascript.info/closure)
 * [JavaScript 的静态作用域链与“动态”闭包链](https://juejin.cn/post/6957913856488243237)
 * [What is the scope of variables in JavaScript?](https://stackoverflow.com/questions/500431/what-is-the-scope-of-variables-in-javascript)
+* [Execution Context, Lexical Environment, and Closures in JavaScript](https://betterprogramming.pub/execution-context-lexical-environment-and-closures-in-javascript-b57c979341a5)
